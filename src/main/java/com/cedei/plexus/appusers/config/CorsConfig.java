9ffed65487fbe915f9f1932c3d3f0b81b26f5a8f
Ayoze Martín Hdernández <ayozemartinhdez@gmail.com>
2018-10-26 08:03:42 +0100

    configurado el acceso cors

---
+++ src/main/java/com/cedei/plexus/appusers/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.cedei.plexus.appusers.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * CorsConfig
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:8081");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+        return bean;
+    }
+    
+}
